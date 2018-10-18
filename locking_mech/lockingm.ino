2374dc1606444264042dde20d1d2d2a5949388a3
Nidhin Harilal <nidhin.harilal@iitgn.ac.in>
2018-10-18 14:41:17 +0530

    Add files via upload

---
+++ locking_mech/lockingm.ino
@@ -0,0 +1,22 @@
+char password = 'a';
+char terminal;
+void setup() 
+{
+pinMode(9, OUTPUT);
+Serial1.begin(9600);
+digitalWrite(9, HIGH);  //By default, lock is active(locked)
+
+
+}
+
+void loop() {
+  if(Serial1.available()>0) 
+  {terminal = Serial1.read();
+  Serial1.println(terminal);
+  if(terminal == password){
+  digitalWrite(9,LOW);
+  Serial1.println("open");
+  }
+  }
+
+}
