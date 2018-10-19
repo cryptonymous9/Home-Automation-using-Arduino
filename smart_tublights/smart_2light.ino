85917e4cb309a034cba3f5a2af7b384269503720
Nidhin Harilal <nidhin.harilal@iitgn.ac.in>
2018-10-19 14:17:50 +0530

    Add files via upload

---
+++ smart_tublights/smart_2light.ino
@@ -0,0 +1,29 @@
+int tb1 = 3;
+int tb2 = 6;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(tb1,OUTPUT);
+  pinMode(tb2,OUTPUT);
+}
+
+void loop()
+{
+  unsigned int AnalogValue;
+  AnalogValue = analogRead(0);
+  Serial.println(AnalogValue);  
+  if (AnalogValue<180){
+    digitalWrite(tb1,LOW);
+    digitalWrite(tb2,LOW);
+  }
+  else if(AnalogValue>=180 and AnalogValue<=240){
+    digitalWrite(tb1,HIGH);
+    digitalWrite(tb2,LOW);
+    delay(2000);
+  }
+  else{
+    digitalWrite(tb1,HIGH);
+    digitalWrite(tb2,HIGH);
+    delay(2000);
+  }
+}
